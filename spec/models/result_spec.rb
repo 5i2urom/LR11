357533adbc9eb296dad287306b832272956b7177
RomchAss <gameathome2015@gmail.com>
2022-12-12 19:11:59 +0300

    added testing adding to db

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -3,5 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Result, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:str_params) { '6 3 5' }
+  let(:num_params) { 3 }
+  let(:params) {{ num: num_params, str: str_params }}
+  it 'checking db: adding and selecting' do    
+    elem = Result.new(params)
+    expect(elem.save).to eq(true)
+    expect(Result.find_by(str: str_params).nil?).to eq(false)
+    Result.last.destroy
+  end
 end
